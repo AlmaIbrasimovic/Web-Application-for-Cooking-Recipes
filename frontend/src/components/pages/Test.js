d7568f3442436ee3cc0cd266035a31b59f8958b6
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2020-09-17 15:02:11 +0200

    Deleted front

--- frontend/src/components/pages/Test.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react'
-
-export default class Test extends Component {
-    render() {
-        return (
-            <h1>{this.props.name}</h1>
-        );
-    }
-}
